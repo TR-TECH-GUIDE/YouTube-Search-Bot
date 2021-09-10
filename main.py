cc8ddb42fe022ad6630c8c8f1442b6b7ddb6170f
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-10 16:13:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,8 +54,8 @@ async def search(bot, update):
         channel_link = result["channel"]["link"]
         video_link = result["link"]
         description = f"{views_short} | {duration}"
-        details = f"**Title:** {title}" + "\n" \
-        f"**Video Link:** {video_link}" + "\n" \
+        details = f"{video_link}" + "\n" \
+        f"**Title:** {title}" + "\n" \
         f"**Channel:** [{channel_name}]({channel_link})" + "\n" \
         f"**Duration:** {duration_text}" + "\n" \
         f"**Views:** {views}" + "\n" \
