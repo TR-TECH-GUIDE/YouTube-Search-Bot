c3be62b0a32429382568ad3875759f9f806d38c5
Tharuk Renuja <73170853+TR-TECH-GUIDE@users.noreply.github.com>
2021-09-10 15:43:02 +0530

    YouTube-Search-Bot

---
+++ README.md
@@ -0,0 +1,39 @@
+# YouTube-Search-Bot
+
+A youtube search telegram bot.
+
+---
+
+```
+Made with Python3
+(C) @TR-TECH-GUIDE
+Copyright permission under MIT License
+License -> https://github.com/TR-TECH-GUIDE/YouTube-Search-Bot/blob/main/LICENSE
+```
+
+---
+
+## Deploy
+
+```sh
+git clone https://github.com/TR-TECH-GUIDE/YouTube-Search-Bot/tree/main
+cd YouTube-Search-Bot
+pip3 install -r requirements.txt
+# <Create Variables appropriately>
+python3 main.py
+```
+
+---
+
+## Variables
+
+- `API_HASH` Your API Hash from my.telegram.org
+- `API_ID` Your API ID from my.telegram.org
+- `BOT_TOKEN` Your bot token from @BotFather
+
+---
+
+## Credits
+
+- [Tharuk Renuja](https://github.com/TR-TECH-GUIDE)
+- [Pyrogram](https://github.com/pyrogram/pyrogram)
---
+++ main.py
@@ -0,0 +1,83 @@
+# Made with python3
+# (C) @TR-TECH-GUIDE
+# Copyright permission under MIT License
+# All rights reserved by TR-TECH-GUIDE
+# License -> https://github.com/TR-TECH-GUIDE/YouTube-Search-Bot/blob/main/LICENSE
+
+import os
+import ytthumb
+import requests
+from requests.utils import requote_uri
+from pyrogram import Client, filters
+from pyrogram.types import *
+
+
+Bot = Client(
+    "YouTube-Search-Bot",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
+
+
+@Bot.on_message(filters.private & filters.all)
+async def text(bot, update):
+    text = "Search youtube videos using below buttons.\n\nMade by @SLBotsOfficial"
+    reply_markup = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton(text="Search here", switch_inline_query_current_chat="")],
+            [InlineKeyboardButton(text="Search in another chat", switch_inline_query="")]
+        ]
+    )
+    await update.reply_text(
+        text=text,
+        reply_markup=reply_markup,
+        disable_web_page_preview=True,
+        quote=True
+    )
+
+
+@Bot.on_inline_query()
+async def search(bot, update):
+    results = requests.get(
+        "https://youtube.api.fayas.me/videos/?query=" + requote_uri(update.query)
+    ).json()["result"][:50]
+    answers = []
+    for result in results:
+        title = result["title"]
+        views_short = result["viewCount"]["short"]
+        duration = result["duration"]
+        duration_text = result["accessibility"]["duration"]
+        views = result["viewCount"]["text"]
+        publishedtime = result["publishedTime"]
+        channel_name = result["channel"]["name"]
+        channel_link = result["channel"]["link"]
+        description = f"{views_short} | {duration}"
+        details = f"**Title:** {title}" + "\n" \
+        f"**Channel:** [{channel_name}]({channel_link})" + "\n" \
+        f"**Duration:** {duration_text}" + "\n" \
+        f"**Views:** {views}" + "\n" \
+        f"**Published Time:** {publishedtime}" + "\n" \
+        "\n" + "**Made by @TR-TECH-GUIDE**"
+        thumbnail = ytthumb.thumbnail(result["id"])
+        reply_markup = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text="Watch Video 📹", url=result["link"])]
+            ]
+        )
+        try:
+            answers.append(
+                InlineQueryResultPhoto(
+                    title=title,
+                    description=description,
+                    caption=details,
+                    photo_url=thumbnail,
+                    reply_markup=reply_markup
+                )
+            )
+        except:
+            pass
+    await update.answer(answers)
+
+
+Bot.run()
